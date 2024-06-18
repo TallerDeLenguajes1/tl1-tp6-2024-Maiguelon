69d654842b382328d5ed87f0461f052a0ee9680a
Maiguelon <miguel_carretero@outlook.com>
2024-06-18 15:51:24 -0300

    Terminado el punto 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+
 Console.WriteLine("Hello, World!");
 int a;
 int b;
@@ -6,3 +8,31 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b); 
+
+*/
+
+// Ejercicio 1
+
+// Console.WriteLine("Introduzca un number");
+// string number = Console.ReadLine();
+
+// if (int.TryParse(number, out int numero))
+// {
+//     int inverso = 0;
+//     while (numero > 0)
+//     {
+//         int digito = numero % 10;
+//         numero /= 10;
+//         inverso = inverso * 10 + digito;
+//     }
+
+//     Console.WriteLine($"\nNumero invertido: {inverso}\n");
+
+// }
+// else
+// {
+//     Console.WriteLine("Eso no es un numero >:|");
+// }
+
+// Ejercicio 2:
+
